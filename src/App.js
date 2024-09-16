9667ce7159841bfc6a44d0d15e4117fe31a892f9
Akash Kundu <aakassh.uit@gmail.com>
2024-09-16 10:21:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import StoryList from "./components/StoryList";
+
+const App = () => {
+  const [stories, setStories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch top stories from FastAPI backend
+    const fetchStories = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/top-stories");
+        if (!response.ok) {
+          throw new Error("Failed to fetch stories");
+        }
+        const data = await response.json();
+        setStories(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStories();
+  }, []);
+
+  if (loading) {
+    return <div className="flex h-screen items-center justify-center text-3xl font-semibold"> Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="flex h-screen items-center justify-center text-3xl font-semibold">Something went wrong!! {error}</div>;
+  }
+
+
+  return (
+    <div>
+      <div className="flex justify-center mt-4">
+        <h1 className="font-bold text-3xl sm:text-lg">Top 10 Hacker News Stories</h1>
+      </div>
+      <div className="flex justify-center mt-6">
+        <StoryList stories={stories} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StoryList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function StoryList({ stories }) {
+  return (
+    <ul>
+      {stories.map((story, index) => (
+        <li key={index}>
+          <div className="border border-slate-600 p-4 m-2 bg-slate-100">
+            <a className="flex justify-center text-xl font-semibold mb-1 pb-2" href={story.url} target="_blank" rel="noopener noreferrer">
+              {story.title}
+            </a>
+            <p className="flex justify-center text-md font-semibold">Author: {story.author}</p>
+            <p className="flex justify-center text-md font-semibold">Score: {story.score}</p>
+            <p className="flex justify-center text-md font-semibold">Time: {story.time} IST</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default StoryList;
